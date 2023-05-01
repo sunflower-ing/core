c8e3bf89dc9047ef8f8eb7c8bf537d8db46d06d0
Alexey Lazarev <xom4ek@gmail.com>
2023-05-01 13:10:36 +0200

    fix: CSRF start from https

--- sunflower/settings.py
+++ sunflower/settings.py
@@ -31,7 +31,7 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', ["*"])
-CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', ["*"])
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', ["https://*"])
 
 # Application definition
 
