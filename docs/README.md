1b0d3abb8d2eef2c90a80a095829047faae8dbe1
Aleksandr Mironov <a.m.mironov@gmail.com>
2023-12-04 21:15:49 +0400

    Remove docs

--- docs/README.md
+++
@@ -1,4 +0,0 @@
-# Simple mkdocs exmaple
-
-
-Here a little exmaple about how you can serve your docs via mkdocs
--- ocsp/views.py
+++ ocsp/views.py
@@ -1,6 +1,7 @@
 from cryptography.x509 import ocsp
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions, viewsets
 from rest_framework.response import Response
 
@@ -123,6 +124,8 @@ class SourceViewSet(viewsets.ModelViewSet):
     queryset = Source.objects.all()
     serializer_class = SourceSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["name", "host", "addr"]
 
     def create(self, request, *args, **kwargs):
         instance = None
