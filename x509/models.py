e5cb9ec8821fb4658feb692dc22cf53669dc2a64
Aleksandr Mironov <a.m.mironov@gmail.com>
2024-01-30 15:53:12 +0400

    fix: heh

--- x509/models.py
+++ x509/models.py
@@ -331,11 +331,11 @@ class CRL(models.Model):
         return crl_from_pem(self.body.encode())
 
     def as_pem(self) -> str:
-        if self.next_update >= timezone.now():
+        if self.next_update <= timezone.now():
             self.save()
         return self.body
 
     def as_der(self) -> bytes:
-        if self.next_update >= timezone.now():
+        if self.next_update <= timezone.now():
             self.save()
         return crl_to_der(self.as_object())
